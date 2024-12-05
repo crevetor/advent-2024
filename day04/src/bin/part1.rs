3988206e21126ca81db6768cdac6039469644cca
Antoine Reversat <a.reversat@gmail.com>
2024-12-04 21:04:05 -0500

    Day 4

---
+++ day04/src/bin/part1.rs
@@ -0,0 +1,60 @@
+use std::env;
+use std::fs;
+use std::process;
+use matrix::Matrix;
+
+fn read_input(filename: &str) -> Vec<String> {
+    let mut ret: Vec<String> = Vec::new();
+    let content = fs::read_to_string(filename).expect("Unable to read from file.");
+    for line in content.lines() {
+        ret.push(line.trim().to_string());
+    }
+
+    ret
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Wrong number of args");
+        process::exit(1);
+    }
+
+    let contents = read_input(&args[1]);
+    let mat = Matrix::new(contents.iter().map(|s| s.chars().collect()).collect());
+    let mut num_xmas = 0;
+
+    for y in 0..mat.num_rows() {
+        let line = mat.row(y).unwrap().iter().collect::<String>();
+        let diag = mat.diag([0, y], [1, 1]).unwrap().iter().collect::<String>();
+        let other_diag = mat.diag([mat.num_cols() - 1, y], [-1, 1]).unwrap().iter().collect::<String>();
+        println!("row {y}");
+        println!("{line:?}");
+        println!("{diag:?}");
+        println!("{other_diag:?}");
+        num_xmas += line.matches("XMAS").count();
+        num_xmas += diag.matches("XMAS").count();
+        num_xmas += other_diag.matches("XMAS").count();
+        num_xmas += line.chars().rev().collect::<String>().matches("XMAS").count();
+        num_xmas += diag.chars().rev().collect::<String>().matches("XMAS").count();
+        num_xmas += other_diag.chars().rev().collect::<String>().matches("XMAS").count();
+    }
+    for x in 0..mat.num_cols() {
+        let col = mat.col(x).unwrap().iter().collect::<String>();
+        let diag = mat.diag([x, 0], [1, 1]).unwrap().iter().collect::<String>();
+        let other_diag = mat.diag([x, 0], [-1, 1]).unwrap().iter().collect::<String>();
+        println!("col {x}");
+        println!("{col:?}");
+        println!("{diag:?}");
+        println!("{other_diag:?}");
+        num_xmas += col.matches("XMAS").count();
+        num_xmas += col.chars().rev().collect::<String>().matches("XMAS").count();
+        if x > 0 && x < mat.num_cols() - 1 {
+            num_xmas += diag.matches("XMAS").count();
+            num_xmas += other_diag.matches("XMAS").count();
+            num_xmas += diag.chars().rev().collect::<String>().matches("XMAS").count();
+            num_xmas += other_diag.chars().rev().collect::<String>().matches("XMAS").count();
+        }
+    }
+    println!("{}", num_xmas);
+}
---
+++ day04/src/bin/part2.rs
@@ -0,0 +1,41 @@
+use std::env;
+use std::fs;
+use std::process;
+use matrix::Matrix;
+use anyhow::Result;
+
+fn read_input(filename: &str) -> Vec<String> {
+    let mut ret: Vec<String> = Vec::new();
+    let content = fs::read_to_string(filename).expect("Unable to read from file.");
+    for line in content.lines() {
+        ret.push(line.trim().to_string());
+    }
+
+    ret
+}
+
+fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Wrong number of args");
+        process::exit(1);
+    }
+
+    let contents = read_input(&args[1]);
+    let mat = Matrix::new(contents.iter().map(|s| s.chars().collect()).collect());
+    let mut num_xmas = 0;
+
+    for y in 1..mat.num_rows()-1 {
+        for x in 1..mat.num_cols()-1 {
+            let sub = mat.get_sub_matrix(x, y, [3,3])?;
+            let diag = sub.diag([0, 0], [1, 1]).unwrap();
+            let other_diag = sub.diag([2, 0], [-1, 1]).unwrap();
+            if (diag.iter().collect::<String>() == "MAS" || diag.iter().rev().collect::<String>() == "MAS") && (other_diag.iter().collect::<String>() == "MAS" || other_diag.iter().rev().collect::<String>() == "MAS") {
+                num_xmas += 1;
+            }
+        }
+    }
+    println!("{num_xmas}");
+
+    Ok(())
+}
